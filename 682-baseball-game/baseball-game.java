41915ae4a9cc610b4ad871a53d5bd228dea62c26
Anshika <anshika7427@gmail.com>
2026-08-19 16:46:17 +0530

    Time: 3 ms (80.77%) | Memory: 43.6 MB (49.56%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,22 +1,27 @@
 class Solution {
     public int calPoints(String[] operations) {
-        ArrayList<Integer> lst = new ArrayList<>();
-        
-        for(int i = 0 ; i < operations.length ; i++){
+        Stack<Integer> st = new Stack<>();
+        int n = operations.length;
+        for(int i = 0 ; i < n ; i++){
             if(operations[i].equals("C")){
-                lst.remove(lst.size()-1);
+                st.pop();
             }else if(operations[i].equals("D")){
-                lst.add(lst.get(lst.size() - 1) * 2);
+                int num = st.peek() * 2;
+                st.push(num);
             }else if(operations[i].equals("+")){
-                int n = lst.size() - 1;
-                lst.add(lst.get(n)+lst.get(n-1));
+                int first = st.pop();
+                int second = st.pop();
+
+                int sum = first + second;
+                st.push(second);
+                st.push(first);
+                st.push(sum);
             }else{
-                int n = Integer.parseInt(operations[i]);
-                lst.add(n);
+                st.push(Integer.parseInt(operations[i]));
             }
         }
-        int sum = 0;
-        for(int num : lst){
+        int sum = 0 ;
+        for(int num : st){
             sum = sum + num;
         }
         return sum;
